6028dbde42424b4fa86ef1ce92a0f151824a9bbb
Piseth Nem <76490169+itsmedevseth@users.noreply.github.com>
2023-08-05 15:14:25 +0700

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+<script>
+    function test1(){
+        document.getElementById('IMG').setAttribute('src','https://phorn-khouch.netlify.app/image/h1.png')
+    }
+    function test2(){
+        document.getElementById('IMG').setAttribute('src','https://phorn-khouch.netlify.app/image/shirt4.png')
+    }
+    function test3(){
+        document.getElementById('IMG').setAttribute('src','https://phorn-khouch.netlify.app/image/shirt2.png')
+    }
+    function test4(){
+        document.getElementById('IMG').setAttribute('src','https://phorn-khouch.netlify.app/image/w1.png')
+    }
+    function defaulIMG(){
+        document.getElementById('IMG').setAttribute('src','https://phorn-khouch.netlify.app/image/w3.png')
+    }
+</script>
